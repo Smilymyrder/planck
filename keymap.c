174204ed1552230cb37eb5fae45841bc4b80cfb2
Smilymyrder <smilymyrder@gmail.com>
2017-09-15 21:33:57 +0200

    added workworkworkman layer
    think about using wokrman as default

--- keymap.c
+++ keymap.c
@@ -11,21 +11,23 @@
 extern keymap_config_t keymap_config;
 
 #define Qwerty 0  // default layer
-#define Num    1  // numbers + arrowkeys (+ deadkeys?)
-#define Fn     3  // functions and navigation to other layers
+#define Work   1  // workworkworkworkwork
+#define Num    2  // numbers + arrowkeys (+ deadkeys?)
+#define Fn     4  // functions and navigation to other layers
 
-#define Fkeys  2  // raised numberslayer for f-keys
+#define Fkeys  3  // raised numberslayer for f-keys
 
 #define Pad    x  // numpad
 
 // Switching layers
 #define qtyl  TO(Qwerty) // go to default layer - if needed
+#define wrkl  TO(Work)   // go to workworkworkworkwork
 #define numl  MO(Num)    //momentary num layer
 #define fkyl   MO(Fkeys)  // momentary f-keys layer
 // #define fn1   MO(Fn)     //momentary function layer
 
 // tap toggle fn layer
-#define fn1   TT(Fn)      // layer tap-toggle
+#define funl   TT(Fn)      // layer tap-toggle
 #define TAPPING_TOGGLE 2  //defines number of taps needed
 
 // Fillers to make layering more clear
@@ -42,15 +44,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |  Fn  |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ,   |  .   |  /   |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | Num  |Enter |Space | Bspc |Shift |      |      |      |      |
+ * | Ctrl | GUI  | Alt  | Num  |Enter |Space | Bspc |Shift |  [   |  ]   |  -   |  =   |
  * `-----------------------------------------------------------------------------------'
  */
 [Qwerty] = {
   { KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, ___},
   { KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
-  { fn1, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, ___},
-  { KC_LCTRL, KC_LGUI, KC_LALT, numl, KC_ENT, KC_SPC, KC_BSPC, KC_LSFT, ___, ___, ___, ___}
+  { funl, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, ___},
+  { KC_LCTL, KC_LGUI, KC_LALT, numl, KC_ENT, KC_SPC, KC_BSPC, KC_LSFT, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL}
 },
+
+/* Workman
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   Q  |   D  |   R  |   W  |   B  |   J  |   F  |   U  |   P  |   ;  |  \   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Tab  |   A  |   S  |   H  |   T  |   G  |   Y  |   N  |   E  |   O  |   I  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |  Fn  |   Z  |   X  |   M  |   C  |   V  |   K  |   L  |   ,  |   .  |   /  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | GUI  | Alt  | Num  |Enter |Space | Bspc |Shift |  [   |  ]   |  -   |  =   |
+ * `-----------------------------------------------------------------------------------'
+ */
+[Work] = {
+  { KC_ESC, KC_Q, KC_D, KC_R, KC_W, KC_B, KC_J, KC_F, KC_U, KC_P, KC_SCLN, KC_BSLASH},
+  { KC_TAB, KC_A, KC_S, KC_H, KC_T, KC_G, KC_Y, KC_N, KC_E, KC_O, KC_I, KC_QUOT},
+  { funl, KC_Z, KC_X, KC_M, KC_C, KC_V, KC_K, KC_L, KC_COMM, KC_DOT, KC_SLSH, ___},
+  { KC_LCTL, KC_LGUI, KC_LALT, numl, KC_ENT, KC_SPC, KC_BSPC, KC_LSFT, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL}
+},
+
 
 /* Numbers
  * ,-----------------------------------------------------------------------------------.
